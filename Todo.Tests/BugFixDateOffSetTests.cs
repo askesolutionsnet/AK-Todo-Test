becae7b56b6136946811f9235907f619633635e1
Abdul Khan <askesolutions.net@gmail.com>
2023-06-18 17:39:18 +0100

    review dateoffset and add more unit test

--- Todo.Tests/BugFixDateOffSetTests.cs
+++ Todo.Tests/BugFixDateOffSetTests.cs
@@ -70,13 +70,13 @@ namespace Todo.Tests
         }
 
         [TestMethod]
-        public async Task API_List_Should_Return_OkResult_With_Correct_Created_Field()
+        public async Task API_List_Should_Return_With_Correct_Created_Field_DateTimeOffSet_Success()
         {
             // Arrange
             var todoItems = new List<TodoItem>
             {
-                new TodoItem { Id = Guid.NewGuid(), Created = DateTime.UtcNow, Text = "Item 1", Completed = null },
-                new TodoItem { Id = Guid.NewGuid(), Created = DateTime.UtcNow, Text = "Item 2", Completed = null }
+                new TodoItem { Id = Guid.NewGuid(), Created = DateTime.Now.ToUniversalTime(), Text = "Item 1", Completed = null },
+                new TodoItem { Id = Guid.NewGuid(), Created = DateTime.Now.ToUniversalTime(), Text = "Item 2", Completed = null }
             };
 
             mockSender
@@ -91,14 +91,45 @@ namespace Todo.Tests
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.AreEqual(200, result.StatusCode);
             Assert.IsNotNull(items);
             Assert.AreEqual(todoItems.Count, items.Count());
 
             // Assert Created field correctness
             foreach (var item in items)
             {
-                Assert.AreEqual(DateTime.UtcNow.Date, item.Created.Date);
+                Assert.AreEqual(DateTimeKind.Utc, item.Created.Kind);
+            }
+        }
+
+        [TestMethod]
+        public async Task API_List_Should_Return_With_InCorrect_Created_Field_DateTimeOffSet_Fail()
+        {
+            // Arrange
+            var todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid(), Created = DateTime.Now, Text = "Item 1", Completed = null },
+                new TodoItem { Id = Guid.NewGuid(), Created = DateTime.Now, Text = "Item 2", Completed = null }
+            };
+
+            mockSender
+                .Setup(sender => sender.Send(It.IsAny<ListTodoItemsRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(todoItems);
+
+            var controller = new TodoController(mockSender.Object);
+
+            // Act
+            var result = await controller.List(DataSort.Ascending, ItemsVisibility.Show_All) as OkObjectResult;
+            var items = result.Value as IEnumerable<TodoItem>;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(items);
+            Assert.AreEqual(todoItems.Count, items.Count());
+
+            // Assert Created field correctness
+            foreach (var item in items)
+            {
+                Assert.AreNotEqual(DateTimeKind.Utc, item.Created.Kind);
             }
         }
 
